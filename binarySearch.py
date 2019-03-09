44eb58e9fd980e28603179c126fa95dc86c66307
Hazem Essam <hazemessamm@users.noreply.github.com>
2019-03-09 21:27:52 +0200

    Create binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,20 @@
+def binarySearch(arr, head, tail, x):
+    if tail >= head:
+        mid = head + (tail - 1)//2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binarySearch(arr, head, mid-1, x)
+        else:
+            return binarySearch(arr, mid+1, tail, x)
+    else:  
+        return 0
+
+arr = [ 2, 3, 4, 10, 40 ] 
+x = 10
+result = binarySearch(arr, 0, len(arr)-1, x)
+if result != 0:
+    print("Element found at index", result)
+else:
+    print("Element is not in the array")
